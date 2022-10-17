eb8621cd0ab828bd26086d51fb80cb03dc0257cf
Pramuditha <pramuditha.niroshan@circles.asia>
2022-10-17 22:55:43 +0530

    Add cart component

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import classes from 'Cart.module.css'
+
+const Cart = (props) => {
+    const cartItems = [
+        {
+            id: 'c1',
+            name: "Suchi",
+            amount: 2,
+            price: 12.99
+        }
+    ].map((item) => (<li>{item.name}</li>))
+  return (
+    <div>
+        <ul className={classes['cart-items']}>
+            {cartItems}
+        </ul>
+        <div className={classes.total}>
+            <span>Total Amount</span>
+            <span>35.20</span>
+        </div>
+        <div className={classes.actions}>
+            <button className={classes['button--alt']}>Close</button>
+            <button className={classes.button}>Order</button>
+        </div>
+    </div>
+  )
+}
+
+export default Cart
