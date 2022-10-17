b8eba43d0b49fcf1e4219436953b48b866785a12
Pramuditha <pramuditha.niroshan@circles.asia>
2022-10-17 17:36:21 +0530

    Meal summary text and available meals component creation

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AvailableMeals from './AvailableMeals'
+import MealSummary from './MealSummary'
+
+const Meals = () => {
+  return (
+    <>
+        <MealSummary/>
+        <AvailableMeals/>
+    </>
+  )
+}
+
+export default Meals
